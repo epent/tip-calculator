27bb45052dcc6cd391f72adf8881bf28dd7c23ee
epent <pentium95@mail.ru>
2021-11-17 17:53:48 +0200

    add "reset" button

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const numberOfPeople = document.querySelector(".section-people input");
 const tipPercent = 15;
 const tipAmount = document.querySelectorAll(".section-output__amount")[0];
 const totalAmount = document.querySelectorAll(".section-output__amount")[1];
+const resetButton = document.querySelector(".section-output button");
 
 function splitBill(event) {
+  // count and output tip
   const tip =
     (Number(enteredBillAmount.value) * Number(tipPercent)) /
     100 /
@@ -14,10 +16,20 @@ function splitBill(event) {
 
   tipAmount.textContent = `$${tip.toFixed(2)}`;
 
+  // count and output total
   const total =
     Number(enteredBillAmount.value) / Number(numberOfPeople.value) + tip;
 
   totalAmount.textContent = `$${total.toFixed(2)}`;
 }
 
+function resetBill() {
+  console.log("clicked");
+  enteredBillAmount.value = "";
+  numberOfPeople.value = "";
+  tipAmount.textContent = "$0.00";
+  totalAmount.textContent = "$0.00";
+}
+
 numberOfPeople.addEventListener("change", (event) => splitBill(event));
+resetButton.addEventListener("click", resetBill);
