c8ba896ac2761e3ddc2c4b6db96f4db0a62af7de
epent <pentium95@mail.ru>
2021-11-17 18:31:57 +0200

    tip buttons change tip%

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 "use strict";
 
 const enteredBillAmount = document.querySelector(".section-bill input");
+const tipButtons = document.querySelectorAll(".section-tip__buttons button");
+let tipPercent = 0;
 const numberOfPeople = document.querySelector(".section-people input");
-const tipPercent = 15;
 const tipAmount = document.querySelectorAll(".section-output__amount")[0];
 const totalAmount = document.querySelectorAll(".section-output__amount")[1];
 const resetButton = document.querySelector(".section-output button");
 
-function splitBill(event) {
+function activateButton(button) {
+  tipButtons.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+  button.setAttribute("class", "active");
+
+  switch (button.textContent) {
+    case "5%":
+      tipPercent = 5;
+      break;
+    case "10%":
+      tipPercent = 10;
+      break;
+    case "15%":
+      tipPercent = 15;
+      break;
+    case "25%":
+      tipPercent = 25;
+      break;
+    case "50%":
+      tipPercent = 50;
+      break;
+  }
+}
+
+function splitBill() {
   // count and output tip
   const tip =
     (Number(enteredBillAmount.value) * Number(tipPercent)) /
@@ -29,7 +55,17 @@ function resetBill() {
   numberOfPeople.value = "";
   tipAmount.textContent = "$0.00";
   totalAmount.textContent = "$0.00";
+
+  tipButtons.forEach((btn) => {
+    btn.classList.remove("active");
+  });
 }
 
-numberOfPeople.addEventListener("change", (event) => splitBill(event));
+numberOfPeople.addEventListener("change", splitBill);
 resetButton.addEventListener("click", resetBill);
+
+tipButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    activateButton(button);
+  });
+});
